8cd544ea8ed180e7893b76f92075b07d91a1717b
root <root@localhost.localdomain>
2021-10-23 16:11:56 -0600

    adding database

---
+++ src/main/java/com/jaloway/jalowayheroku/Book.java
@@ -0,0 +1,49 @@
+package com.jaloway.jalowayheroku;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String author;
+	
+	private String title;
+	
+	private String paragraphs;
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getParagraphs() {
+		return paragraphs;
+	}
+
+	public void setParagraphs(String paragraphs) {
+		this.paragraphs = paragraphs;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+}
--- src/main/java/com/jaloway/jalowayheroku/HelloController.java
+++ src/main/java/com/jaloway/jalowayheroku/HelloController.java
@@ -1,14 +1,19 @@
 package com.jaloway.jalowayheroku;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	@Autowired
+	BookRepo bookRepo;
 
 	int bookChunkSize = 3;
 	
@@ -18,9 +23,12 @@ public class HelloController {
 		return "Greetings from Novlr! Get mapping. Page number: " + pageNumber;
 	}
 		
-	@PostMapping("/")
-	public String post() {
+	@PostMapping("/addbook")
+	public String post(@RequestBody Book book) {
 		System.out.println("Endpoint Reached. Post mapping.");
+		System.out.println("author: " + book.getAuthor());
+		System.out.println("title: " + book.getTitle());
+		bookRepo.saveAndFlush(book);
 		return "Greetings from Novlr! Post mapping.";
 	}
 	
