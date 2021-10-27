105b304769cc72f379ea2d5945bc3905330c6c5d
ajtpxn <ajtpxn@gmail.com>
2021-10-26 20:38:21 -0600

    updates to routes

--- src/main/java/com/jaloway/jalowayheroku/BookController.java
+++ src/main/java/com/jaloway/jalowayheroku/BookController.java
@@ -1,5 +1,7 @@
 package com.jaloway.jalowayheroku;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,21 +11,38 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 @RestController
-public class HelloController {
+public class BookController {
 	
 	@Autowired
 	BookRepo bookRepo;
 
 	int bookChunkSize = 3;
 	
+	@GetMapping("/")
+	public String index() {
+		System.out.println("Endpoint Reached. Index mapping.");
+		List<Book> list = bookRepo.findAll();
+		for (Book book : list) {
+			System.out.println("id: " + book.getId());
+			System.out.println("author: " + book.getAuthor());
+			System.out.println("title: " + book.getTitle());
+			System.out.println("paragraphs: " + book.getParagraphs());
+		}
+		return "Greetings from Novlr! Index mapping.";
+	}
+	
 	@GetMapping("/{pageNumber}")
-	public String index(@PathVariable int pageNumber) {
-		System.out.println("Endpoint Reached. Get mapping. Page number: " + pageNumber);
-		return "Greetings from Novlr! Get mapping. Page number: " + pageNumber;
+	public String getbook(@PathVariable int id) {
+		System.out.println("Endpoint Reached. Get mapping.");
+		Book book = bookRepo.getById(id);
+		return "Greetings from Novlr! Get mapping.";
 	}
 		
-	@PostMapping("/addbook")
+	@PostMapping("/")
 	public String post(@RequestBody Book book) {
 		System.out.println("Endpoint Reached. Post mapping.");
 		System.out.println("author: " + book.getAuthor());
@@ -32,16 +51,19 @@ public class HelloController {
 		return "Greetings from Novlr! Post mapping.";
 	}
 	
-	@PatchMapping("/{id}")
-	public String patch(@PathVariable int id) {
+	@PatchMapping(path = "/{id}", consumes = "application/json-patch+json")
+	public String patch(@PathVariable int id, @RequestBody Book book) {
 		System.out.println("Endpoint Reached. Patch mapping. Id: " + id);
+		bookRepo.getById(id);
+		bookRepo.saveAndFlush(book);
 		return "Greetings from Novlr! Patch mapping. Id: " + id;
 	}
 	
 	@DeleteMapping("/{id}")
 	public String delete(@PathVariable int id) {
 		System.out.println("Endpoint Reached. Delete Mapping. Id: " + id);
-		return "Greetings from Novlr! Delete Mapping. Id: " + id;
+		bookRepo.deleteById(id);
+		return "Greetings from Novlr! Deleted book: " + id;
 	}
 
 }
