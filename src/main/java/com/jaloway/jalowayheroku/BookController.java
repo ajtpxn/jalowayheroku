89c3f65def86e858ceaa56d09ff85f759648c8f5
ajtpxn <ajtpxn@gmail.com>
2021-10-26 20:49:30 -0600

    updated cors

--- src/main/java/com/jaloway/jalowayheroku/BookController.java
+++ src/main/java/com/jaloway/jalowayheroku/BookController.java
@@ -3,6 +3,7 @@ package com.jaloway.jalowayheroku;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -11,10 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 @RestController
+@CrossOrigin({"*", "http://localhost:4200"})
 public class BookController {
 	
 	@Autowired
