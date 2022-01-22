1a980037acb499e0d96d02fdd5932473edcee5c2
ajtpxn <ajtpxn@gmail.com>
2022-01-22 14:45:09 -0700

    trying spring sec redirect again

--- src/main/java/com/jaloway/jalowayheroku/WebSecurityConfig.java
+++ src/main/java/com/jaloway/jalowayheroku/WebSecurityConfig.java
@@ -14,8 +14,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 //	  http.authorizeRequests().anyRequest().permitAll();
-	  System.out.println(http.toString()); 
-	  http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+	  System.out.println(http.toString());
+//	  http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
 //	  http.requiresChannel().anyRequest().requiresInsecure();
 //	  http.sessionManagement().sessionFixation().none();
   }
