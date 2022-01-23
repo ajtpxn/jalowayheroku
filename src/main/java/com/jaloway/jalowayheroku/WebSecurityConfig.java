ab1f2b7c54b13a807e9a1e7d672b73c2fd7cb47d
ajtpxn <ajtpxn@gmail.com>
2022-01-23 04:44:02 -0700

    removing spring security

--- src/main/java/com/jaloway/jalowayheroku/WebSecurityConfig.java
+++ src/main/java/com/jaloway/jalowayheroku/WebSecurityConfig.java
@@ -1,23 +1,23 @@
-package com.jaloway.jalowayheroku;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-//	  http.authorizeRequests().anyRequest().permitAll();
-	  System.out.println(http.toString());
-//	  http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
-//	  http.requiresChannel().anyRequest().requiresInsecure();
-//	  http.sessionManagement().sessionFixation().none();
-  }
-}
-
+//package com.jaloway.jalowayheroku;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+////	  http.authorizeRequests().anyRequest().permitAll();
+//	  System.out.println(http.toString());
+////	  http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+////	  http.requiresChannel().anyRequest().requiresInsecure();
+////	  http.sessionManagement().sessionFixation().none();
+//  }
+//}
+//
